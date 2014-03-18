9db724bdf21dcd05d1ce2f5e2eeaeadad24138dd
Marin <marin@stayke.net>
2014-03-17 17:06:26 -0700

    twilio sms

---
+++ twilio/twilio.go
@@ -0,0 +1,134 @@
+package twilio
+
+/*
+
+example:
+
+tc := sms.TwilioCfg{
+    FromPhone: "+XXXX",
+    AccountSID: "XXXX",
+    AuthToken: "XXXX",
+}
+
+err := tc.SMS("+XXXXX", "message")
+
+if err != nil {
+    fmt.Print(err) // log error
+} else {
+    // sent success
+}
+
+*/
+
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+const (
+    apiMsgUrl = "https://api.twilio.com/2010-04-01/Accounts/%s/Messages.json"
+)
+
+//------------------------------------------------------------
+// TwilioCfg
+//------------------------------------------------------------
+
+type TwilioCfg struct {
+	FromPhone  string // outgoing phone
+	AccountSID string // twilio accnt id
+	AuthToken  string // twilio token
+}
+
+//------------------------------------------------------------
+// TwilioCfg methods
+//------------------------------------------------------------
+
+//------------------------------------------------------------
+// Send SMS
+// 
+// silently ignore invalid numbers (numeric only)
+// if twilio response status is not 201
+//    put response body into err and return
+// else return nil assuming success
+//------------------------------------------------------------
+func (tc *TwilioCfg) SMS(toPhone, body string) (err error) {
+
+	var req *http.Request
+	var resp *http.Response
+
+    // silently ignore invalid numbers
+    // numeric only
+    if !validPhone(toPhone) {
+        return nil
+    }
+
+	// append +
+    toPhone = "+" + toPhone
+
+	// trim long msg
+	if len(body) >= 160 {
+		body = body[:155] + "..."
+	}
+
+	form := url.Values{
+		"To": {
+			toPhone,
+		},
+		"From": {
+			tc.FromPhone,
+		},
+		"Body": {
+			body,
+		},
+	}
+
+	postUrl := fmt.Sprintf(apiMsgUrl, tc.AccountSID)
+	req, err = http.NewRequest("POST", postUrl, bytes.NewBufferString(form.Encode()))
+
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.SetBasicAuth(tc.AccountSID, tc.AuthToken)
+
+	client := &http.Client{}
+	resp, err = client.Do(req)
+
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != 201 {
+		respBody, ioerr := ioutil.ReadAll(resp.Body)
+
+		if ioerr != nil {
+			return errors.New("twilio failed sending\nfailed to read http resp")
+		} else {
+			return errors.New("twilio failed sending: " + bytes.NewBuffer(respBody).String())
+		}
+	}
+
+	return nil
+}
+
+func validPhone(phone string) bool {
+
+    if len(phone) == 0 {
+        return false
+    }
+
+    for _, k := range phone {
+        if k < '0' || k > '9' {
+            return false
+        }
+    }
+
+    return true
+}
---
+++ twilio_test.go
@@ -0,0 +1,59 @@
+package alienplugs
+
+import (
+	"fmt"
+	"github.com/deze333/alienplugs/twilio"
+	"github.com/deze333/skini"
+	"os"
+	"testing"
+)
+
+type TwilioParams struct {
+	AccountSID string
+	AuthToken  string
+	FromPhone  string
+    ToPhone    string
+}
+
+func (t *TwilioParams) ok() bool {
+    return t.AccountSID != "" && t.AuthToken != "" && t.FromPhone != "" && t.ToPhone != ""
+}
+
+func TestTwilio(t *testing.T) {
+	var err error
+
+	// Load parameters
+	var tp TwilioParams
+	// Try private data first, if not use public
+	fname := "private_twilio.ini"
+	if _, err := os.Stat(fname); err == nil {
+		err = skini.ParseFile(&tp, fname)
+	} else {
+		fname = "public_twilio.ini"
+		if _, err := os.Stat(fname); err == nil {
+			err = skini.ParseFile(&tp, fname)
+		} else {
+			t.Fatal(fmt.Sprint("No suitable parameters found, exiting"))
+		}
+	}
+
+	if err != nil {
+		t.Fatal(fmt.Sprintf("Error parsing %v: %v", fname, err))
+	}
+
+    if !tp.ok() {
+        t.Fatal(fmt.Sprintf("Error parsing %v: missing parameters for test: \n%+v", fname, tp))
+    }
+
+	tcfg := twilio.TwilioCfg{
+		FromPhone:  tp.FromPhone,
+		AccountSID: tp.AccountSID,
+		AuthToken:  tp.AuthToken,
+	}
+
+    err = tcfg.SMS(tp.ToPhone, "unit test message")
+
+    if err != nil {
+        t.Fatal(fmt.Sprintf("Failed sending twilio message:\n%s", err))
+    }
+}
